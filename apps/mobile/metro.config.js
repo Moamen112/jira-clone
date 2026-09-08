78c4b1795b5335666559fb16a8ba186ee150dada
Mo'men Shehab <65092885+Moamen112@users.noreply.github.com>
2026-09-08 14:33:36 +0300

    Fix: App start bug

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -6,11 +6,12 @@ const monorepoRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
-// 1. Watch all files within the monorepo
-config.watchFolders = [monorepoRoot];
+// 1. Watch all files within the monorepo while preserving defaults
+config.watchFolders = [...(config.watchFolders || []), monorepoRoot];
 
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
+  ...(config.resolver.nodeModulesPaths || []),
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
 ];
